334ac50476755abf3fa96f4a062db57c8f08fca1
Gomtesh9 <93309863+Gomtesh9@users.noreply.github.com>
2022-02-27 19:09:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r=document.getElementById("rate").value;
+y=document.getElementById("years").value;
+a=Number(p)+(p*y*r/100);
+b=new Date().getFullYear();
+b=Number(b)+Number(y);
+if(p==""){
+alert("please enter amount:");
 }
+if(r==""){
+alert("please enter rate:");
+}
+if(y==""){
+alert("please enter number of years:")
+}
+if(p<=0){
+alert("please enter a positive number:");
+}
+document.write("If you deposit <mark>"+p+"</mark> <br> at an interest rate of <mark>"+r+"%</mark>. </br> You will receive an amount of <mark>"+a+"</mark>. <br> In the year <mark>"+b+"</mark>");
+}
+
         
